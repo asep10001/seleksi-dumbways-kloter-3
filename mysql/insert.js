7a18acb16d4457ba10d3ce7c1e8c18229c146071
Asep Agus <a2h2hemingway@gmail.com>
2020-06-27 18:15:59 +0000

    menambahkan beberapa styling untuk tampilannya

--- mysql/insert.js
+++ mysql/insert.js
@@ -4,10 +4,10 @@ db.getConnection(function(err) {
     if (err) throw err;
     
     let sql = 
-    // `INSERT INTO provinsi_tb (nama, diresmikan, photo, pulau) 
-    // VALUES ("Sumatera Barat", "1 Oktober 1945", 'https://upload.wikimedia.org/wikipedia/commons/6/62/Coat_of_arms_of_West_Sumatra.svg', "Sumatera")`;
-    // `INSERT INTO kabupaten_tb (Nama, Provinsi_id, Diresmikan, photo) 
-    // VALUES ("Kabupaten Bandung", 1, "20 April 1641", "./assets/images/kabupaten-bandung.jpg")`;
+    `INSERT INTO provinsi_tb (nama, diresmikan, photo, pulau) 
+    VALUES ("Sumatera Barat", "1 Oktober 1945", 'https://upload.wikimedia.org/wikipedia/commons/6/62/Coat_of_arms_of_West_Sumatra.svg', "Sumatera")`;
+    `INSERT INTO kabupaten_tb (Nama, Provinsi_id, Diresmikan, photo) 
+    VALUES ("Kabupaten Bandung", 1, "20 April 1641", "./assets/images/kabupaten-bandung.jpg")`;
 
     db.query(sql, function (err, result) {
         if (err) throw err;
